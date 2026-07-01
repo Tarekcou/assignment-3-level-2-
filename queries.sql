c7f16bbe1a283b6a011d1d702a898f76c62111e0
tarek <sitarek77@gmail.com>
2026-07-01 14:13:37 +0600

    final

--- queries.sql
+++ queries.sql
@@ -11,7 +11,6 @@ select booking_id, user_id, match_id, coalesce(payment_status,'Action Required')
 where payment_status is null 
 
 4.
-
 select b.booking_id, u.full_name, m.fixture, b.total_cost::INT from bookings b
 join users u on b.user_id= u.user_id
 join matches m on b.match_id=m.match_id
@@ -21,7 +20,6 @@ select u.user_id,u.full_name, b.booking_id from users u
 left join bookings b on u.user_id=b.user_id
 
 6.
-
 select booking_id,match_id,total_cost::INT from bookings
 where total_cost> (select avg(total_cost) from bookings)
 
